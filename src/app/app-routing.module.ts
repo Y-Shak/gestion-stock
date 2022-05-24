63cb5c8e302e46f7574d60f0cfd23166d603ee67
Y-Shak <82205070+Y-Shak@users.noreply.github.com>
2022-05-24 16:01:39 +0200

    feat( us dashboard) : mise en place du routing et construction page dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
 import {PageLoginComponent} from "./pages/page-login/page-login.component";
+import { PageStatistiquesComponent } from './pages/page-statistiques/page-statistiques.component';
 
 const routes: Routes = [
-  {path : '', component : PageLoginComponent}
+  {path : 'login', component : PageLoginComponent},
+  {path : 'inscrire', component : PageInscriptionComponent},
+  {
+    path : '', 
+    component : PageDashboardComponent,
+    children: [
+      {path : 'statistiques', component : PageStatistiquesComponent}
+    ]
+  },
 ];
 
 @NgModule({
